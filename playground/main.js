1170cae83a3f90a5d0c24e579e479b2cf3a233a9
Kyle Feng <kylefeng28@gmail.com>
2018-03-24 00:08:07 -0400

    Default to example if nothing saved

--- playground/main.js
+++ playground/main.js
@@ -1,35 +1,5 @@
 'use strict';
 
-// Init canvas
-let canvas = new GCanvas(500, 500);
-
-// Init Ace editor
-let editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.session.setMode("ace/mode/javascript");
-
-// Load
-readFromLocalStorage(editor);
-
-/* Keybindings */
-// Save keybinding
-editor.commands.addCommand({
-	name: 'save',
-	exec: function() {
-		saveToLocalStorage(editor);
-	},
-	bindKey: { mac: 'Cmd-S', win: 'Ctrl-S' }
-});
-
-// Eval keybinding
-editor.commands.addCommand({
-	name: 'eval',
-	exec: function() {
-		evalSelectionOrLine(editor);
-	},
-	bindKey: { mac: 'Cmd-Enter', win: 'Ctrl-Enter' }
-});
-
 const example = `mandelShader = new MandelbrotShader();
 imgShader = new ImageShader("/matrix/img/spock.png");
 
@@ -49,5 +19,37 @@ draw = async function() {
 	// await canvas.drawRect(new GRect(250, 250, 500, 500), new GPaint("#4D4E53"));
 }
 `;
+
+// Init canvas
+let canvas = new GCanvas(500, 500);
+
+// Init Ace editor
+let editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+
+// Load saved or example
+if (!readFromLocalStorage(editor)) {
+	loadExample(editor, example);
+}
+
+/* Keybindings */
+// Save keybinding
+editor.commands.addCommand({
+	name: 'save',
+	exec: function() {
+		saveToLocalStorage(editor);
+	},
+	bindKey: { mac: 'Cmd-S', win: 'Ctrl-S' }
+});
+
+// Eval keybinding
+editor.commands.addCommand({
+	name: 'eval',
+	exec: function() {
+		evalSelectionOrLine(editor);
+	},
+	bindKey: { mac: 'Cmd-Enter', win: 'Ctrl-Enter' }
+});
 
 window.p = canvas._p;
